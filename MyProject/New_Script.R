58251c939602b449694f67cbdd945f415726af0b
Emily Rios <riose3@livemail.uthscsa.edu>
2019-04-16 09:57:45 -0500

    adding New_Script

---
+++ MyProject/New_Script.R
@@ -0,0 +1,2 @@
+personalizeTemplate('~/Desktop/hispanic%2520discrepancy%2520study.fields_Final.csv',title = 'UD Disparities',author = 'EMILY RIOS'
+                    + 	,packages=c('GGally','ggplot2','tableone'));
